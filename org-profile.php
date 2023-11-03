9f6768dd570916f778429590c9fc798170aeba46
Gauresh <gauresh.bagayatkar@gmail.com>
2023-11-03 18:11:08 +0530

    Fixed login bug

--- org-profile.php
+++ org-profile.php
@@ -1,56 +1,61 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Organizer Profile</title>
-    <!-- Meta -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">    
-    <link rel="shortcut icon" href="VClogo.png">  
-    <!-- FontAwesome JS-->
-    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
-    
-    <!-- Global CSS -->
-     <link href="css/myvals.min.css" rel="stylesheet">
-    <!-- Plugins CSS -->    
-    <link rel="stylesheet" href="assets/plugins/prism/prism.css">
-    <style>
-        .parallax {
-          /* The image used */
-          background-image: url("pexels-pixabay-45842.jpg");
-          
-          /* Set a specific height */
-          min-height: 500px; 
-        
-          /* Create the parallax scrolling effect */
-          background-attachment: fixed;
-          background-position: center;
-          background-repeat: no-repeat;
-          background-size: cover;
-        }
-    </style>
-</head> 
-  <body>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-   
-    <!-- Navbar --> 
-    <header id="header " class="header">                          
-        <nav id="main-nav" class="main-nav navbar navbar-expand-md bg-primary ">
-            <div class="container-fluid text-white">
-            <a class="navbar-brand logo scrollto" href="LandingPage.html">
-                <span class="logo-title text-white">VolunteerConnect</span>
-            </a>  
-            </div>
-            <form action = "logout.php">
-            <button class="btn btn-secondary" type="submit">
-            Logout</button>
-            </form>
-        </nav><!--//main-nav-->
-    </header>
-  
+
+<head>
+  <title>Organizer Profile</title>
+  <!-- Meta -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="VClogo.png">
+  <!-- FontAwesome JS-->
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet'
+    type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+  <!-- Global CSS -->
+  <link href="css/myvals.min.css" rel="stylesheet">
+  <!-- Plugins CSS -->
+  <link rel="stylesheet" href="assets/plugins/prism/prism.css">
+  <style>
+    .parallax {
+      /* The image used */
+      background-image: url("pexels-pixabay-45842.jpg");
+
+      /* Set a specific height */
+      min-height: 500px;
+
+      /* Create the parallax scrolling effect */
+      background-attachment: fixed;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  </style>
+</head>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+    crossorigin="anonymous"></script>
+
+  <!-- Navbar -->
+  <header id="header " class="header">
+    <nav id="main-nav" class="main-nav navbar navbar-expand-md bg-primary ">
+      <div class="container-fluid text-white">
+        <a class="navbar-brand logo scrollto" href="LandingPage.html">
+          <span class="logo-title text-white">Pledge</span>
+        </a>
+      </div>
+      <form action="logout.php">
+        <button class="btn btn-secondary" type="submit">
+          Logout</button>
+      </form>
+    </nav><!--//main-nav-->
+  </header>
+
   <div class="avatar1 parallax">
     <h1 style="position: absolute; top: 350px;left: 350px; color: beige;">We connect you with good volunteers to
       help<br>you with your good cause</h1>
@@ -67,15 +72,15 @@
     $sql = "SELECT id, Event_name, Event_location, Event_date, Event_organizer_id, Event_description, Volunteers_required, Volunteers_ready, Event_image FROM org_event_post";
     $resultset = mysqli_query($conn, $sql) or die("database error:" . mysqli_error($conn));
     while ($record = mysqli_fetch_assoc($resultset)) {
-     
-    
-     ?>
+
+
+      ?>
       <div class="card hovercard p-5 w-50 border-0 shadow-sm">
         <div class="cardheader">
           <div class="avatar">
-           <?php 
-           $data=$record['Event_name']?>
-            <img src="display.php?event_name=<?php echo $data;?>" style="width:180px; height:200px">
+            <?php
+            $data = $record['Event_name'] ?>
+            <img src="display.php?event_name=<?php echo $data; ?>" style="width:180px; height:200px">
           </div>
         </div>
         <div class="card-body info">
@@ -122,8 +127,8 @@
           </ul>
         </div>
       </div>
-     
-  <?php }
+
+    <?php }
     ?>
 
   </div>
